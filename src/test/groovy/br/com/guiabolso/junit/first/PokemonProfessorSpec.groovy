14c543443eedddf33e585e1f2f4a180fb02d3c15
Joao Enomoto <joao@guiabolso.com.br>
2017-06-01 14:52:56 -0300

    test ok

--- src/test/groovy/br/com/guiabolso/junit/first/PokemonProfessorSpec.groovy
+++ src/test/groovy/br/com/guiabolso/junit/first/PokemonProfessorSpec.groovy
@@ -4,8 +4,37 @@ import spock.lang.Specification
 
 class PokemonProfessorSpec extends Specification {
 
-    def 'test'() {
-        expect:
-        false
+    PokemonProfessor oak
+    
+    def setup() {
+        oak = new PokemonProfessor()
+    }
+    
+    def 'pegar charmander'() {
+        given: 'depois de pegar 2 pokes'
+        oak.getPokemon()
+        oak.getPokemon()
+        
+        when: 'pego outro'
+        def charmander = oak.getPokemon()
+        
+        then: 'deve ser um chamander'
+        assert charmander == "Charmander"
+    }
+    
+    def 'nao ha mais pokemons pra pegar'() {
+        given: 'depois de pegar 6 pokemons'
+        oak.getPokemon()
+        oak.getPokemon()
+        oak.getPokemon()
+        oak.getPokemon()
+        oak.getPokemon()
+        oak.getPokemon()
+        
+        when: 'pego outro'
+        oak.getPokemon()
+        
+        then: 'nao posso conseguir'
+        thrown IllegalStateException
     }
 }
